a352e75d9955e00d7fe987f2015281ebeefdb29e
pandeyankit9473-max <pandeyankit9473@gmail.com>
2026-03-30 22:03:39 +0530

    ak

---
+++ codeforces/A_Goals_of_Victory.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+       int sum=0;
+        for(int i=0;i<n-1;i++){
+            cin>>a[i];
+            sum+=a[i];
+        }
+        cout<<-sum<<endl;
+    }
+}
